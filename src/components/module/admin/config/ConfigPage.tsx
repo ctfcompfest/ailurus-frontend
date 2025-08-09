43d8dfe1446b20cc27078b0f31fb2dd225c85300
Muhammad Faishol Amirul Mukminin <faishol.amirul27@gmail.com>
2025-08-10 05:00:25 +0700

    feat: grouping config

--- src/components/module/admin/config/ConfigPage.tsx
+++ src/components/module/admin/config/ConfigPage.tsx
@@ -50,6 +50,60 @@ export default function ConfigPage() {
       }),
   });
 
+  const ConfigInlineEdit = ({config_key, value}: {config_key: string, value: string}) => {
+    return (
+      <div className="grid grid-cols-7 gap-4 mb-3" key={config_key}>
+        <React.Fragment>
+          <div className="flex flex-row col-span-2 items-center">
+            <strong>{config_key}:</strong>
+          </div>
+          <div className="flex flex-row items-center col-span-4 w-full">
+            {editingKey === config_key ? (
+              <input
+                className="input input-bordered w-full"
+                value={editedValue}
+                onChange={(e) => setEditedValue(e.target.value)}
+              />
+            ) : (
+              <pre>{value}</pre>
+            )}
+          </div>
+          <div className="flex flex-row items-center gap-2">
+            {editingKey === config_key ? (
+              <>
+                <button
+                  className="btn btn-primary btn-sm"
+                  onClick={() => handleSave(config_key)}
+                >
+                  Save
+                </button>
+                <button
+                  className="btn btn-secondary btn-sm"
+                  onClick={handleCancel}
+                >
+                  Cancel
+                </button>
+              </>
+            ) : (
+              <button
+                className="btn btn-secondary btn-sm"
+                onClick={() => handleEdit(config_key, value)}
+              >
+                Edit
+              </button>
+            )}
+          </div>
+        </React.Fragment>
+      </div>
+    )
+  };
+
+  const CONTEST_INFO_KEY = ["EVENT_NAME", "LOGO_URL", "SERVICE_MODE", "UNLOCK_MODE"];
+  const CONTEST_SESSION_KEY = ["NUMBER_ROUND", "NUMBER_TICK", "TICK_DURATION"];
+  const CONTEST_TIME_KEY = ["START_TIME", "ATTACK_TIME", "FREEZE_TIME", "IS_CONTEST_PAUSED"];
+  const CONTEST_FLAG_KEY = ["FLAG_FORMAT", "FLAG_RNDLEN"];
+  const CONTEST_PROGRESS_KEY = ["CURRENT_TICK", "CURRENT_ROUND", "LAST_TICK_CHANGE", "FREEZE_TICK", "FREEZE_ROUND", "LAST_PAUSED"];
+  const EXCLUDED_KEY = [...CONTEST_INFO_KEY, ...CONTEST_SESSION_KEY, ...CONTEST_TIME_KEY, ...CONTEST_PROGRESS_KEY, ...CONTEST_FLAG_KEY];
   return (
     <div className="px-4 mb-5">
       <div className="flex flex-row justify-between mb-4">
@@ -62,7 +116,67 @@ export default function ConfigPage() {
           Flag, submission, game progress will be <u>deleted</u>. Are you sure?
         </ConfirmModal>
       </div>
-      {Object.entries(contestConfig).map(([key, value]) => (
+      <div className="my-5">
+        <div className="bg bg-neutral rounded-md px-4 py-2 my-2 flex flex-row justify-between items-center gap-4">
+          <div className="flex flex-col gap-2">
+            <strong className="font-strong text-lg">General</strong>
+          </div>
+        </div>
+        {CONTEST_INFO_KEY.filter((key) => key in contestConfig).map(
+          (key) => <ConfigInlineEdit key={key} config_key={key} value={contestConfig[key]} />
+        )}
+      </div>
+      <div className="my-5">
+        <div className="bg bg-neutral rounded-md px-4 py-2 my-2 flex flex-row justify-between items-center gap-4">
+          <div className="flex flex-col gap-2">
+            <strong className="font-strong text-lg">Time</strong>
+          </div>
+        </div>
+        {CONTEST_TIME_KEY.filter((key) => key in contestConfig).map(
+          (key) => <ConfigInlineEdit key={key} config_key={key} value={contestConfig[key]} />
+        )}
+      </div>
+      <div className="my-5">
+        <div className="bg bg-neutral rounded-md px-4 py-2 my-2 flex flex-row justify-between items-center gap-4">
+          <div className="flex flex-col gap-2">
+            <strong className="font-strong text-lg">Contest Progress</strong>
+          </div>
+        </div>
+        {CONTEST_PROGRESS_KEY.filter((key) => key in contestConfig).map(
+          (key) => <ConfigInlineEdit  key={key} config_key={key} value={contestConfig[key]} />
+        )}
+      </div>
+      <div className="my-5">
+        <div className="bg bg-neutral rounded-md px-4 py-2 my-2 flex flex-row justify-between items-center gap-4">
+          <div className="flex flex-col gap-2">
+            <strong className="font-strong text-lg">Game Stage</strong>
+          </div>
+        </div>
+        {CONTEST_SESSION_KEY.filter((key) => key in contestConfig).map(
+          (key) => <ConfigInlineEdit key={key} config_key={key} value={contestConfig[key]} />
+        )}
+      </div>
+      <div className="my-5">
+        <div className="bg bg-neutral rounded-md px-4 py-2 my-2 flex flex-row justify-between items-center gap-4">
+          <div className="flex flex-col gap-2">
+            <strong className="font-strong text-lg">Flag</strong>
+          </div>
+        </div>
+        {CONTEST_FLAG_KEY.filter((key) => key in contestConfig).map(
+          (key) => <ConfigInlineEdit key={key} config_key={key} value={contestConfig[key]} />
+        )}
+      </div>
+      <div className="my-5">
+        <div className="bg bg-neutral rounded-md px-4 py-2 my-2 flex flex-row justify-between items-center gap-4">
+          <div className="flex flex-col gap-2">
+            <strong className="font-strong text-lg">Other</strong>
+          </div>
+        </div>
+        {Object.entries(contestConfig).filter(([key, value]) => EXCLUDED_KEY.indexOf(key) === -1).map(
+          ([key, value]) => <ConfigInlineEdit  key={key} config_key={key} value={value} />
+        )}
+      </div>
+      {/* {Object.entries(contestConfig).map(([key, value]) => (
         <div className="grid grid-cols-7 gap-4 mb-3" key={key}>
           <React.Fragment>
             <div className="flex flex-row col-span-2 items-center">
@@ -106,7 +220,7 @@ export default function ConfigPage() {
             </div>
           </React.Fragment>
         </div>
-      ))}
+      ))} */}
     </div>
   );
 }
