9baede9a55454b3c5f371f5c94a8b1d706707adf
Daffa Abhipraya <daffaabhiprayaputra@gmail.com>
2025-09-27 23:21:00 +0700

    fix: hotfix xmlns

--- src/components/module/attackmap/AttackMapPanel.tsx
+++ src/components/module/attackmap/AttackMapPanel.tsx
@@ -1,62 +1,66 @@
-import { ServerMode } from "@/types/common";
-import { Team } from "@/types/team";
+import type { Team } from "@/types/team";
 import { getCoordinates } from "./utils";
+import { useId } from "react";
 
 interface AttackMapPanelProps {
-  teamData?: Team[];
+	teamData?: Team[];
 }
 
 interface AttackMapPointProps {
-  pointId: number;
-  pointName: string;
-  teamLen: number;
+	pointId: number;
+	pointName: string;
+	teamLen: number;
 }
 
 function AttackMapPoint({ pointId, pointName, teamLen }: AttackMapPointProps) {
-  const pointSize = 100;
-  const labelGapSize = 30;
-  const { posX, posY } = getCoordinates(pointId, teamLen);
+	const pointSize = 100;
+	const labelGapSize = 30;
+	const { posX, posY } = getCoordinates(pointId, teamLen);
 
-  return (
-    <g>
-      <image
-        href={"/entity-team.png"}
-        width={pointSize}
-        height={pointSize}
-        x={posX}
-        y={posY}
-      />
-      <foreignObject
-        x={posX + pointSize / 2 - 70}
-        y={posY + pointSize + labelGapSize - 32}
-        width={140}
-        height={100}
-      >
-        <div
-          xmlns="http://www.w3.org/1999/xhtml"
-          className="map-entity px-1 text-center"
-          style={{ wordBreak: "break-word", overflowWrap: "anywhere", lineHeight: 1.1 }}
-        >
-          {pointName}
-        </div>
-      </foreignObject>
-    </g>
-  );
+	return (
+		<g>
+			<image
+				href={"/entity-team.png"}
+				width={pointSize}
+				height={pointSize}
+				x={posX}
+				y={posY}
+			/>
+			<foreignObject
+				x={posX + pointSize / 2 - 70}
+				y={posY + pointSize + labelGapSize - 32}
+				width={140}
+				height={100}
+			>
+				<div
+					className="map-entity px-1 text-center"
+					style={{
+						wordBreak: "break-word",
+						overflowWrap: "anywhere",
+						lineHeight: 1.1,
+					}}
+				>
+					{pointName}
+				</div>
+			</foreignObject>
+		</g>
+	);
 }
 
 export function AttackMapPanel({ teamData }: AttackMapPanelProps) {
-  const teamLen = teamData?.length ?? 1;
+	const teamLen = teamData?.length ?? 1;
+	const entityId = useId();
 
-  return (
-    <g id="entity">
-      {teamData?.map((data, idx) => (
-        <AttackMapPoint
-          pointId={idx}
-          pointName={data.name}
-          teamLen={teamLen}
-          key={"ent-point-" + idx}
-        />
-      ))}
-    </g>
-  );
+	return (
+		<g id={entityId}>
+			{teamData?.map((data, idx) => (
+				<AttackMapPoint
+					pointId={idx}
+					pointName={data.name}
+					teamLen={teamLen}
+					key={"ent-point-" + data.id}
+				/>
+			))}
+		</g>
+	);
 }
